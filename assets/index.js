cddb0fdaec4aeb0184d966293d5ebbc085439bb7
Jdark91 <=>
2020-04-28 22:23:48 -0500

    Sexta clase Javascript - This

--- assets/index.js
+++ assets/index.js
@@ -5,6 +5,11 @@ const video = document.querySelector('video')
 const buttonPlay = document.querySelector('#play-pause');
 const buttonUnmuteMute = document.querySelector('#unmute-mute');
 
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+const player = new MediaPlayer({ 
+    el: video, 
+    plugins: [
+        //new AutoPlay()
+    ] 
+});
 buttonPlay.onclick = () => player.togglePlayPause();
 buttonUnmuteMute.onclick = () => player.toggleUnmuteMute();
